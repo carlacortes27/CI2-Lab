3aabba44409779106bf0abe837ea35e288fa87ad
GeraltdeBilbao <geraltdebilbao98@gmail.com>
2026-05-28 11:41:58 +0200

    Logo

--- client/src/components/ui/CompanyLogo.jsx
+++ client/src/components/ui/CompanyLogo.jsx
@@ -10,45 +10,41 @@
 import { useState } from 'react';
 import { T, brandColor, initials } from '../../styles/theme.js';
 
-// Mapa de nombre de empresa → dominio para Clearbit Logo API
-const COMPANY_DOMAINS = {
-  'mckinsey':            'mckinsey.com',
-  'mckinsey & company':  'mckinsey.com',
-  'iberdrola':           'iberdrola.com',
-  'deloitte':            'deloitte.com',
-  'ferrovial':           'ferrovial.com',
-  'banco santander':     'santander.com',
-  'santander':           'santander.com',
-  'telefónica':          'telefonica.com',
-  'telefonica':          'telefonica.com',
-  'kpmg':                'kpmg.com',
-  'repsol':              'repsol.com',
-  'bcg':                 'bcg.com',
-  'boston consulting':   'bcg.com',
-  'indra':               'indracompany.com',
-  'bbva':                'bbva.com',
-  'acciona':             'acciona.com',
-  'caixabank':           'caixabank.com',
-  'amazon web services': 'amazonaws.com',
-  'aws':                 'amazonaws.com',
-  'amazon':              'amazon.com',
-  'ey':                  'ey.com',
-  'ernst & young':       'ey.com',
-  'endesa':              'endesa.com',
-  'google':              'google.com',
-  'airbus':              'airbus.com',
-  'accenture':           'accenture.com',
-  'acs':                 'grupoacs.com',
-  'siemens':             'siemens.com',
-  'schneider electric':  'se.com',
-  'schneider':           'se.com',
-  'aura':                'aura-energy.com',
+// URLs directas de logos desde Wikimedia Commons (fuente pública, no bloqueada)
+const COMPANY_LOGOS = {
+  'mckinsey & company':  'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/McKinsey_and_Company.png/320px-McKinsey_and_Company.png',
+  'mckinsey':            'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/McKinsey_and_Company.png/320px-McKinsey_and_Company.png',
+  'iberdrola':           'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Iberdrola_logo.svg/320px-Iberdrola_logo.svg.png',
+  'deloitte':            'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Deloitte.svg/320px-Deloitte.svg.png',
+  'ferrovial':           'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Ferrovial_logo.svg/320px-Ferrovial_logo.svg.png',
+  'banco santander':     'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Logo_Santander_Isologotype.svg/320px-Logo_Santander_Isologotype.svg.png',
+  'santander':           'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Logo_Santander_Isologotype.svg/320px-Logo_Santander_Isologotype.svg.png',
+  'telefónica':          'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Telefonicalogo.svg/320px-Telefonicalogo.svg.png',
+  'telefonica':          'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Telefonicalogo.svg/320px-Telefonicalogo.svg.png',
+  'kpmg':                'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/KPMG_logo.svg/320px-KPMG_logo.svg.png',
+  'repsol':              'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Repsol_logo.svg/320px-Repsol_logo.svg.png',
+  'bcg':                 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/BCG-logo.svg/320px-BCG-logo.svg.png',
+  'indra':               'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Indra_Sistemas_logo.svg/320px-Indra_Sistemas_logo.svg.png',
+  'bbva':                'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/BBVA_2019.svg/320px-BBVA_2019.svg.png',
+  'acciona':             'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Acciona_logo.svg/320px-Acciona_logo.svg.png',
+  'caixabank':           'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/CaixaBank_logo.svg/320px-CaixaBank_logo.svg.png',
+  'amazon web services': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/320px-Amazon_Web_Services_Logo.svg.png',
+  'aws':                 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/320px-Amazon_Web_Services_Logo.svg.png',
+  'ey':                  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/EY_logo_2019.svg/320px-EY_logo_2019.svg.png',
+  'ernst & young':       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/EY_logo_2019.svg/320px-EY_logo_2019.svg.png',
+  'endesa':              'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Endesa_logo.svg/320px-Endesa_logo.svg.png',
+  'google':              'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/320px-Google_2015_logo.svg.png',
+  'airbus':              'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Airbus_Logo_2017.png/320px-Airbus_Logo_2017.png',
+  'accenture':           'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Accenture.svg/320px-Accenture.svg.png',
+  'acs':                 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Logotipo_ACS.svg/320px-Logotipo_ACS.svg.png',
+  'siemens':             'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Siemens-logo.svg/320px-Siemens-logo.svg.png',
+  'schneider electric':  'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Schneider_Electric_2007.svg/320px-Schneider_Electric_2007.svg.png',
+  'schneider':           'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Schneider_Electric_2007.svg/320px-Schneider_Electric_2007.svg.png',
 };
 
 function getLogoUrl(company) {
   const key = company.trim().toLowerCase();
-  const domain = COMPANY_DOMAINS[key];
-  return domain ? `https://logo.clearbit.com/${domain}` : null;
+  return COMPANY_LOGOS[key] || null;
 }
 
 export default function CompanyLogo({
